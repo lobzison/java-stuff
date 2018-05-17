9fcc7c294d64a1a853069c2d6753256c8e67a40e
Gleb <lobzison@gmail.com>
2018-05-17 22:38:09 +0200

    initial api dummy

---
+++ src/Solver.java
@@ -0,0 +1,23 @@
+public class Solver {
+    Board init;
+
+    public Solver(Board initial) {
+        this.init = initial;
+    }         // find a solution to the initial board (using the A* algorithm)
+
+    public boolean isSolvable() {
+        return false;
+    }       // is the initial board solvable?
+
+    public int moves() {
+        return 1;
+    }                     // min number of moves to solve initial board; -1 if unsolvable
+
+    public Iterable<Board> solution() {
+        return init.neighbors();
+    }      // sequence of boards in a shortest solution; null if unsolvable
+
+    public static void main(String[] args) {
+        int i = 0;
+    } // solve a slider puzzle (given below)
+}
